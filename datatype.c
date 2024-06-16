2b4c1a302db6795473fc6520a2221afcebba9d76
MS. Shubhangi Chavan <162903263+Shubhangichavan42@users.noreply.github.com>
2024-06-16 15:39:11 +0530

    Add files via upload

---
+++ datatype.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    float pi=3.12;
+    printf("age is %f \n",pi);
+
+    char star= '*' ;
+    printf("star is %c \n",star);
+
+    int a=22;
+    printf("value is the %d \n",a);
+
+
+    int final_price = 200;
+    printf("final price in tution is the %d \n",final_price);
+    return 0;
+  
+}
